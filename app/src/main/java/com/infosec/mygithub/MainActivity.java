735609bb5a15dc0ad600b29d1f50fd43a15825d8
jasur <bestonly125@gmail.com>
2019-09-19 00:18:45 +0500

    new Main activity

--- app/src/main/java/com/infosec/mygithub/MainActivity.java
+++ app/src/main/java/com/infosec/mygithub/MainActivity.java
@@ -3,12 +3,21 @@ package com.infosec.mygithub;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView textView;
+    EditText editText;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
     }
 }
